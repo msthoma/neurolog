3aa8909507147b2e3ed5fb8a83a8a330df681782
marios <msthoma@yahoo.com>
2021-04-16 15:46:43 +0300

    add smaller than train method

---
+++ src/smaller_than/scenario_train.py
@@ -0,0 +1,24 @@
+import torch
+
+from abducibles import abducibles, exclusive
+from abduction import Abduction
+from manager import SmallerThanManager
+from src.abstract_translator import AbstractTranslator
+from src.networks.mnist_nets import COMP_NET
+from src.params import useGPU, sicstus_bin
+from src.run import scenario_train
+
+network = COMP_NET()
+if useGPU:
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    network.to(device)
+
+translator = AbstractTranslator(abducibles, exclusive)
+dataManager = SmallerThanManager()
+abduction = Abduction(sicstus_bin, translator)
+outputClasses = [10] * 2
+scenario = 'smaller_than'
+
+if __name__ == '__main__':
+    # Train on T1
+    scenario_train(network, outputClasses, translator, dataManager, scenario, abduction, dataset_name="T1_dataset.csv")
