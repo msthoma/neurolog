9d3a38f2b5fe61d76126fc6cc3c725605b237d62
marios <msthoma@yahoo.com>
2021-04-16 18:54:33 +0300

    modify test and train methods to accept dataset names as parameters

--- src/run.py
+++ src/run.py
@@ -13,8 +13,9 @@ from src.train import Trainer
 
 
 def scenario_train(network, outputClasses, translator: AbstractTranslator, dataManager: BenchmarkManager, scenario,
-                   abduction: AbstractAbduction, dataset_name: str = "train_data.txt"):
-    examples = dataManager.loadExampleTuples(data_root + scenario + "/" + dataset_name)
+                   abduction: AbstractAbduction, dataset_name: str = None):
+    examples = dataManager.loadExampleTuples(
+        data_root + scenario + "/" + f"{dataset_name}.csv" if dataset_name is not None else "train_data.txt")
 
     shuffling_index = 0
     while shuffling_index < 10:
@@ -22,7 +23,7 @@ def scenario_train(network, outputClasses, translator: AbstractTranslator, dataM
         shuffling_index += 1
 
     examples = examples[1:number_of_training_examples]
-    model_path = models_root + scenario + '/'
+    model_path = models_root + scenario + "/" + f'{dataset_name}/' if dataset_name is not None else ''
     if not os.path.exists(model_path):
         os.makedirs(model_path)
 
@@ -33,9 +34,15 @@ def scenario_train(network, outputClasses, translator: AbstractTranslator, dataM
 
 
 def scenario_test(network, outputClasses, translator: AbstractTranslator, dataManager: BenchmarkManager, scenario,
-                  model_name, evaluator, dataset_name: str = "test_data.txt"):
-    model_path = models_root + scenario + '/' + model_name
+                  model_name, evaluator, train_dataset: str = None, test_dataset: str = None):
+    model_path = models_root + scenario + "/"
+    if train_dataset is not None:
+        model_path = f"{model_path}{train_dataset}/{model_name}"
+    else:
+        model_path = f"{model_path}/{model_name}"
+    print(model_path)
     network.load_state_dict(torch.load(model_path))
-    examples = dataManager.loadExampleTuples(data_root + scenario + "/" + dataset_name)
+    examples = dataManager.loadExampleTuples(
+        data_root + scenario + "/" + test_dataset if test_dataset is not None else "test_data.txt")
     tester = Tester(network, outputClasses, dataManager, translator, evaluator)
     tester.test(examples)
