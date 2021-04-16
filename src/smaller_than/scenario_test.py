a6727608d3907f522819b316e020dae64e9e22a3
marios <msthoma@yahoo.com>
2021-04-16 16:51:01 +0300

    add test script for smaller_than scenario

---
+++ src/smaller_than/scenario_test.py
@@ -0,0 +1,25 @@
+import torch
+
+from abducibles import abducibles, exclusive
+from evaluate import Evaluator
+from manager import SmallerThanManager
+from src.abstract_translator import AbstractTranslator
+from src.networks.mnist_nets import COMP_NET
+from src.params import useGPU
+from src.run import scenario_test
+
+network = COMP_NET()
+if useGPU:
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    network.to(device)
+
+translator = AbstractTranslator(abducibles, exclusive)
+dataManager = SmallerThanManager()
+outputClasses = [10] * 2
+evaluator = Evaluator()
+scenario = 'smaller_than'
+model_name = 'model_samples_3000_iter_9000_epoch_3.mdl'
+
+if __name__ == '__main__':
+    scenario_test(network, outputClasses, translator, dataManager, scenario, model_name, evaluator,
+                  dataset_name="E1_dataset.csv")
