a2d29b2cb3bf7ea767739e2025085ea6c280519c
marios <msthoma@yahoo.com>
2021-04-17 12:17:40 +0300

    apply new statistics on plots

--- src/smaller_than/visualisation.py
+++ src/smaller_than/visualisation.py
@@ -8,133 +8,42 @@ import pandas as pd
 from local_params import scenario_name
 from src.params import results_root
 
-
-def import_data(filename: str):
-    x, y = list(), list()
-    with open(filename) as fp:
-        while True:
-            line = fp.readline()
-            line = line.rstrip("\n")
-            if not line:
-                break
-
-            data = line.split(' ')
-            x.append(float(data[0]))
-            y.append(float(data[1]))
-
-    return np.array(x), np.array(y)
-
-
-def compute_std(path):
-    arrays = []
-    for scenario in [1, 2, 3, 4, 5]:
-        filename = path + 'accuracy_vs_iter_' + str(scenario) + '.txt'
-        (_, y) = import_data(filename)
-        arrays.append(y)
-
-    a = np.array([arrays[0], arrays[1], arrays[2], arrays[3], arrays[4]])
-    return np.std(a, axis=1)
-
-
-def create_curves(path):
-    arrays = []
-    for scenario in [1, 2, 3, 4, 5]:
-        filename = path + 'accuracy_vs_iter_' + str(scenario) + '.txt'
-        (x, y) = import_data(filename)
-        arrays.append(y)
-
-    ymin = arrays[0]
-    ymax = arrays[0]
-
-    for index in [1, 2, 3, 4]:
-        ymin = np.minimum(ymin, arrays[index])
-        ymax = np.maximum(ymax, arrays[index])
-
-    median = []
-    for index in range(0, len(ymin)):
-        value = np.median([arrays[0][index], arrays[1][index], arrays[2][index], arrays[3][index], arrays[4][index]])
-        median.append(value)
-
-    return x, ymin, ymax, np.array(median)
-
-
 if __name__ == '__main__':
-    # BIGGER_SIZE = 16
-    #
-    # plt.rc('font', size=BIGGER_SIZE)  # controls default text sizes
-    # plt.rc('axes', titlesize=BIGGER_SIZE)  # fontsize of the axes title
-    # plt.rc('axes', labelsize=BIGGER_SIZE)  # fontsize of the x and y labels
-    # plt.rc('xtick', labelsize=BIGGER_SIZE)  # fontsize of the tick labels
-    # plt.rc('ytick', labelsize=BIGGER_SIZE)  # fontsize of the tick labels
-    # plt.rc('legend', fontsize=BIGGER_SIZE)  # legend fontsize
-    # plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
-    #
-    # _, ax = plt.subplots()
-    # ax.set_xlim(0, 9000)
-    # ax.set_ylim(0, 110)
-    #
-    # path1 = results_root + "BSV/"
-    # x, ymin1, ymax1, ymed1 = create_curves(path1)
-    #
-    # path2 = results_root + "ISK/"
-    # x, ymin2, ymax2, ymed2 = create_curves(path2)
-    #
-    # path3 = results_root + "NGA/"
-    # x, ymin3, ymax3, ymed3 = create_curves(path3)
-    #
-    # line1, = ax.plot(x, ymed1, '-')
-    # ax.fill_between(x, ymin1, ymax1, alpha=0.2)
-    #
-    # line2, = ax.plot(x, ymed2, 'g-')
-    # ax.fill_between(x, ymin2, ymax2, facecolor='g', alpha=0.2)
-    #
-    # line3, = ax.plot(x, ymed3, 'r-')
-    # ax.fill_between(x, ymin3, ymax3, facecolor='r', alpha=0.2)
-    #
-    # plt.legend([line1, line2, line3], ["NLOG ?=BSV n=3", "NLOG ?=ISK n=3", "NLOG ?=NGA n=3"], loc='lower right')
-    # plt.savefig('accuracy.png', format='png', bbox_inches='tight')
+    # setup plots
+    fig, axes = plt.subplots(2, 2, figsize=(10, 10))
 
-    fig, axes = plt.subplots(2, 2, figsize=(10, 10))  # , sharex="all", sharey="all")
-
-    # import results files
-    # evaluation_results = defaultdict(lambda: defaultdict(dict))
-    eval_results = defaultdict(dict)
-
+    # import result files
+    test_results = defaultdict(dict)
     for results_file in sorted((Path(results_root) / scenario_name).glob("*.csv")):
         _, train_dataset, _, test_dataset, _, seed = results_file.stem.split("_")
         with open(results_file, "r") as f:
             dt = pd.read_csv(f, sep=",", index_col="iteration")
             dt.columns = [f"iteration_{seed}"]
 
-            if test_dataset in eval_results[train_dataset]:
-                eval_results[train_dataset][test_dataset] = pd.concat([eval_results[train_dataset][test_dataset], dt],
+            if test_dataset in test_results[train_dataset]:
+                test_results[train_dataset][test_dataset] = pd.concat([test_results[train_dataset][test_dataset], dt],
                                                                       axis=1)
             else:
-                eval_results[train_dataset][test_dataset] = dt
+                test_results[train_dataset][test_dataset] = dt
 
-            # evaluation_results[train_dataset][test_dataset][seed] = pd.read_csv(f, sep=",", index_col="iteration")
-            # print(evaluation_results[train_dataset][test_dataset][seed])
-            # break
-    t1e1 = eval_results["T1"]["E1"]
-    t1e1 = pd.concat([t1e1.std(axis=1), t1e1.mean(axis=1), t1e1.median(axis=1), t1e1.min(axis=1), t1e1.max(axis=1)],
-                    axis=1)
-    t1e1.columns = ["std", "mean", "median", "min", "max"]
-    print(t1e1)
-    c = 0
-    for i, (train_dataset, test_datasets) in enumerate(eval_results.items()):
+    # plot results
+    colors = iter(["b", "g", "r", "k"])
+    for i, (train_dataset, test_datasets) in enumerate(test_results.items()):
         for j, (test_dataset, results) in enumerate(test_datasets.items()):
-            print(i, j, train_dataset, test_dataset)
-    #
-    #         x, y = np.array(results["iteration"]), np.array(results["accuracy"])
-    #         axes[i, j].set_ylim(0, 110)
-    #         axes[i, j].set_xlim(0, 9500)
-    #         axes[i, j].plot(x, y, ["b", "g", "r", "y"][c], label="Accuracy % / iteration")
-    #         if i == 1: axes[i, j].set_xlabel("Iterations")
-    #         if j == 0: axes[i, j].set_ylabel(f"Trained with {train_dataset}", fontsize=20)
-    #         if i == 0: axes[i, j].set_title(f"Tested with {test_dataset}", fontsize=20)
-    #         axes[i, j].legend(loc='lower right')
-    #         axes[i, j].tick_params(axis='both', which='major')
-    #         c += 1
-    #
-    # plt.show()
-    # fig.savefig(Path(results_root) / scenario_name / "accuracy.pdf", format="pdf", bbox_inches="tight")
+            results = pd.concat([results.median(axis=1), results.min(axis=1), results.max(axis=1)], axis=1)
+            results.columns = ["median", "min", "max"]
+            color = next(colors)
+            x, y = np.array(results.index), np.array(results["median"])
+            axes[i, j].set_ylim(50, 108)
+            axes[i, j].set_xlim(0, 9500)
+            axes[i, j].plot(x, y, color, linewidth=1, label="Median accuracy (%) / iteration")
+            axes[i, j].fill_between(x, np.array(results["min"]), np.array(results["max"]),
+                                    facecolor=color, alpha=0.3, label="Accuracy values range")
+            if i == 1: axes[i, j].set_xlabel("Iterations")
+            if j == 0: axes[i, j].set_ylabel(f"Trained with {train_dataset}", fontsize=20)
+            if i == 0: axes[i, j].set_title(f"Tested with {test_dataset}", fontsize=20)
+            axes[i, j].legend(loc='lower right', fontsize=8)
+            axes[i, j].tick_params(axis='both', which='major')
+
+    fig.show()
+    fig.savefig(Path(results_root) / scenario_name / "accuracy.pdf", format="pdf", bbox_inches="tight")
